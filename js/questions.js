3f2c5e00ddd123dcb2e50bbbec40a1b2ff3d0b9e
panicor <rachelpanico@hotmail.com>
2021-06-26 13:58:48 -0400

    First commit with all new files and one done html

---
+++ js/questions.js
@@ -0,0 +1,23 @@
+var questions = [
+    {
+      question: "What is Angela's middle name?",
+      choices: ["Karen", "Noelle", "Kurt", "Katherine"],
+      answer: "Noelle",
+    },
+    {
+      question: "What color karate belt of Dwight's dows Jim steal?",
+      choices: ["Purple", "Blue", "Black", "Yellow"],
+      answer: "Purple",
+    },
+    {
+        question: "What is Michael Scott's favorite ice cream flavor?",
+        choices: ["Chunky Monkey", "Vanilla", "Rocky road", "Mint chip"],
+        answer: "Mint chip",
+    },
+    {
+        question: "What is 'Purse Girl''s name?",
+        choices: ["Cammie", "Katherine", "Karla", "Katy"],
+        answer: "Katy",
+    }
+]
+
