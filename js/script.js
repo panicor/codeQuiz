00b7662dcb6ce668595688b43f4da5e0d5b3ee15
panicor <rachelpanico@hotmail.com>
2021-06-26 21:08:14 -0400

    Questions now show on page

--- js/script.js
+++ js/script.js
@@ -1,31 +1,80 @@
 var time = questions.length * 15;
-currentQuestionIndex = 0; 
+var currentQuestionIndex = 0; 
 
 var startButton = document.querySelector("#start-button");
 var questionsEl = document.querySelector("#questions-section");
 var timerEl = document.querySelector("#time");
 var qChoices = document.querySelector("#choices-list");
-var startScreen =  document.querySelector("#start-section");
-var titleEl = document.querySelector("bigbox");
+var startScreen =  document.querySelector(".start-section");
+var titleEl = document.querySelector("#question-title");
+var endSection = document.querySelector("#end-section");
+var scoresList = document.querySelector(".hslist");
+var initials = document.querySelector("#initials");
+var score = document.querySelector("#score");
+var msg = document.querySelector(".rightWrong");
+
 
 function startQuiz (){
-   // startScreen.setAttribute("class", "hidden");
-   // questionsEl.removeAttribute("class");
+    startScreen.setAttribute("class", "hidden");
+    questionsEl.setAttribute("class", "visible");
+    getCurrentQuestion();
     startTimer();
+}
+
+function endQuiz() {
+    startScreen.setAttribute("class", "hidden");
+    questionsEl.setAttribute("class", "hidden");
+    endSection.setAttribute("class", "visible");
+    score.textContent = time;
+}
+
+function goBack(){
+window.history.back();
 }
 
 function getCurrentQuestion(){
  var currentQuestion = questions[currentQuestionIndex];
- titleEl.textContent = currentQuestion.title;
- questionChoices.textContent ="";
+ titleEl.textContent = currentQuestion.question;
+ 
 
- for (let i = 0; i <currentQuestion.choices.length; i++){
+ for (let i = 0; i < currentQuestion.choices.length; i++){
      let choiceNode = document.createElement("button");
      choiceNode.setAttribute("class", "choice");
-     choiceNode.setAttribute("value", currentQuestionChoice[i]);
-     choiceNode.textContent = i + 1 + ". " + currentQuestion.choice[i];
+     choiceNode.setAttribute("value", currentQuestion[i]);
+     choiceNode.textContent = i + 1 + ". " + currentQuestion.choices[i];
      qChoices.appendChild(choiceNode);
  }
+}
+
+function eval (){
+    if (qChoices == questions.answer){
+        msg.textContent = "Correct";
+        nextQuestion();
+    }
+    if (qChoices != questions.answer){
+      time = time - 15;
+      msg.textContent = "Incorrect";
+      nextQuestion();
+      }
+    }
+
+function nextQuestion(){
+    for (let i = 0; i < questions.length; i++){
+    var nextQuestion = questions[currentQuestionIndex + i];
+    titleEl.textContent = questions[currentQuestionIndex + i].question;
+    qChoices.textContent = "";
+
+    for (let i = 0; i < nextQuestion.choices.length; i++){
+        let choiceNode1 = document.createElement("button");
+        choiceNode1.setAttribute("class", "choice");
+        choiceNode1.setAttribute("value", nextQuestion[i]);
+        choiceNode1.textContent = i + 1 + ". " + nextQuestion.choices[i];
+        qChoices.appendChild(choiceNode1);
+    }
+
+   
+} 
+//msg.textContent = "";
 }
 
 function startTimer() {
@@ -34,26 +83,28 @@ function startTimer() {
       time--;
       timerEl.textContent = time;
 
-     if (time == 0) {
+     if (time <= 0) {
+      time = 0;
       clearInterval(timer);
-     // endQuiz();
+      endQuiz();
       }
-    //     // Tests if win condition is met
-    //     if (isWin && timerCount > 0) {
-    //       // Clears interval and stops timer
-    //       clearInterval(timer);
-    //       winGame();
-    //     }
-    //   }
-  
-    //   // Tests if time has run out
-    //   if (timerCount === 0) {
-    //     // Clears interval
-    //     clearInterval(timer);
-    //     loseGame();
-     // }
+      
+     
+
     }, 1000);
   }
+  
+function submitInit(){
+    localStorage.setItem("initials", initals);
+    var initials = localStorage.getItem("initials");
+    hsList.appendChild(initials);
+}
+
+function clearScores(){
+    localStorage.clear();
+}
 
 
+qChoices.addEventListener("click", eval);
 startButton.addEventListener("click", startQuiz);
+
