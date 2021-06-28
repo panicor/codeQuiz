b62af70530f93d86ca06089801510b0f77ab205c
panicor <rachelpanico@hotmail.com>
2021-06-28 18:13:47 -0400

    used .value to get value of input

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-var time = questions.length * 1;
+var time = questions.length * 15;
 var currentQuestionIndex = 0; 
+var correctAnswers = [2,1,4,4];
 
 var startButton = document.querySelector("#start-button");
 var questionsEl = document.querySelector("#questions-section");
@@ -13,11 +14,11 @@ var initials = document.querySelector("#initials");
 var score = document.querySelector("#score");
 var msg = document.querySelector(".rightWrong");
 var submitButton = document.querySelector("#submit-button");
-var tableElInit = document.querySelector(".initSet");
-var tableElScore = document.querySelector(".scoreSet");
+var tableElInit = document.querySelectorAll(".initSet");
+var tableElScore = document.querySelectorAll(".scoreSet");
 
-function startQuiz (){
-    
+function startQuiz(event){
+    event.preventDefault();
     startScreen.setAttribute("class", "hidden");
     questionsEl.setAttribute("class", "visible");
     getCurrentQuestion();
@@ -35,7 +36,9 @@ function goBack(){
 window.history.back();
 }
 
+
 function getCurrentQuestion(){
+
  var currentQuestion = questions[currentQuestionIndex];
  titleEl.textContent = currentQuestion.question;
  
@@ -47,30 +50,15 @@ function getCurrentQuestion(){
      choiceNode.textContent = i + 1 + ". " + currentQuestion.choices[i];
      qChoices.appendChild(choiceNode);
  }
- qChoices.addEventListener("click", eval);
+ 
 }
-
-function eval(){
-    if (rightClick == questions.answer){
-        msg.textContent = "Correct";
-        nextQuestion();
-    }
-    if (rightClick != questions.answer){
-      time = time - 15;
-      msg.textContent = "Incorrect";
-      nextQuestion();
-      }
-    }
 
 function nextQuestion(){
     for (let i = 0; i < questions.length; i++){
-    var nextQuestion = questions[currentQuestionIndex + 1];
-    titleEl.textContent = questions[currentQuestionIndex + 1].question;
-    qChoices.addEventListener("click", eval);
+    var nextQuestion = questions[currentQuestionIndex + i];
+    titleEl.textContent = questions[currentQuestionIndex + i].question;
     qChoices.textContent = "";
-    
         }
-    
 
     for (let i = 0; i < nextQuestion.choices.length; i++){
         let choiceNode1 = document.createElement("button");
@@ -82,7 +70,22 @@ function nextQuestion(){
     //msg.textContent = "";
 } 
 
-
+function eval(){
+ //if (rightClick == correctAnswers[currentQuestionIndex]){
+   if(eventListen == correctAnswers[currentQuestionIndex]){
+        msg.textContent = "Correct";
+   }
+    //if (rightClick != correctAnswers[currentQuestionIndex])
+    if (eventListen != correctAnswers[currentQuestionIndex])
+    {
+      time = time - 15;
+      msg.textContent = "Incorrect";
+      }  
+      getCurrentQuestion();
+       //nextQuestion();
+    }
+    
+    
 function startTimer() {
     // Sets timer
       var timer = setInterval(function() {
@@ -91,31 +94,44 @@ function startTimer() {
 
      if (time <= 0) {
       time = 0;
+      timerEl.textContent = 0;
       clearInterval(timer);
       endQuiz();
       }
     }, 1000);
   }
   
-function submitInit(){
+function submitInit(event){
+    event.preventDefault();
+
+    //gets letters they put in
+    localStorage.setItem("initials", initials.value);
+    localStorage.setItem("score", parseInt(time));
+    var newNew2 =  localStorage.getItem("initials").value;
+    var newNew1 = localStorage.getItem("score").value;
+
     location.href= "hs.html";
-    var initials = localStorage.getItem("initials");
-    var setInit = localStorage.setItem("tableElInit", initials);
-    var setScore = localStorage.setItem("scoresList", time);
-    tableElInit.appendChild(setInit);
-    tableElScore.appendChild(setScore);
+    
+    tableElInit.textContent = newNew2;
+    tableElScore.textContent = newNew1;
+    
+    // appendChild(inits);
+    // tableElScore.appendChild(setScore1);
 }
 
 function clearScores(){
     localStorage.clear();
 }
 
+if(submitButton){
+submitButton.addEventListener("click", submitInit);
+}
 
-for (let i = 0; i < qChoices.length; i++){
-var rightClick = qChoices.addEventListener("click", eval);
+if(startButton){
+startButton.addEventListener("click", startQuiz);
 }
 
 
-submitButton.addEventListener("click", submitInit);
-startButton.addEventListener("click", startQuiz);
-
+if(qChoices){
+var eventListen = qChoices.addEventListener("click", eval);
+}
