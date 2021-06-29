8fc89b224fdc5615bd916775f034306e1d145358
panicor <rachelpanico@hotmail.com>
2021-06-29 18:49:46 -0400

    eval working and updated readme

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 //global variables and arrays
 var time = questions.length * 15;
 var currentQuestionIndex = 0; 
-var correctAnswers = [2,1,4,4];
 
 //grabs elements from html
 var startButton = document.querySelector("#start-button");
@@ -40,6 +39,7 @@ function endQuiz() {
     startScreen.setAttribute("class", "hidden");
     questionsEl.setAttribute("class", "hidden");
     endSection.setAttribute("class", "visible");
+    
     score.textContent = time;
 }
 
@@ -48,60 +48,68 @@ function goBack(){
 window.history.back();
 }
 
+//evaluates buttons value to corract answers in array
+function eval(event){
+    event.preventDefault();
+
+    if(event.target.textContent === questions[currentQuestionIndex].answer){
+          msg.textContent = "Correct"; 
+          currentQuestionIndex++;
+          if (currentQuestionIndex < questions.length){
+             getCurrentQuestion();
+          } else {
+                endQuiz()
+          }
+         
+    } else {
+        
+        time = time - 15;
+        msg.textContent = "Incorrect"; 
+        currentQuestionIndex++;
+        if (currentQuestionIndex < questions.length){
+            getCurrentQuestion();
+        } else {
+             endQuiz()
+        }
+    }
+ }
 
 function getCurrentQuestion(){
 
  var currentQuestion = questions[currentQuestionIndex];
+ console.log('curretnQuestion-->',currentQuestion)
  titleEl.textContent = currentQuestion.question;
+
 
    b1.setAttribute('content', '1');
    b1.setAttribute('class', 'button');  
    b1.textContent = currentQuestion.choices[0];
+    b1.addEventListener("click", eval);
    qChoices.appendChild(b1);
 
   
    b2.setAttribute('content', '2');
    b2.setAttribute('class', 'button');  
    b2.textContent = currentQuestion.choices[1];
+   b2.addEventListener("click", eval);
    qChoices.appendChild(b2);
    
 
    b3.setAttribute('content', '3');
    b3.setAttribute('class', 'button');  
    b3.textContent = currentQuestion.choices[2];
+   b3.addEventListener("click", eval);
    qChoices.appendChild(b3);
    
 
    b4.setAttribute('content', '4');
    b4.setAttribute('class', 'button');  
    b4.textContent = currentQuestion.choices[3];
+   b4.addEventListener("click", eval);
    qChoices.appendChild(b4);
 
 }
 
-//evaluates buttons value to corract answers in array
-function eval(event){
-event.preventDefault();
-if(b1Click && (b1.content == correctAnswers[currentQuestionIndex]) || (b2Click && b2.content == correctAnswers[currentQuestionIndex]) || (b3Click && b3.content == correctAnswers[currentQuestionIndex]) || (b4 &&b4.content == correctAnswers[currentQuestionIndex])){
-   // if(currentQuestion.choices[i] == correctAnswers[currentQuestionIndex] || b2.content == correctAnswers[currentQuestionIndex] || b3.content == correctAnswers[currentQuestionIndex] || b4.content == correctAnswers[currentQuestionIndex]){
-  // if(b1Click && (b1.textContent == questions[currentQuestionIndex].answer) || b2Click && (b2.textContent == questions[currentQuestionIndex].answer) || b3Click && (b3.textContent == questions[currentQuestionIndex]) || b4Click && (b4.textContent ==questions[currentQuestionIndex].answer)){
-     // for (let i = 0; i < questions.length; i++){
-   // if(currentQuestion.choices[i] == correctAnswers[currentQuestionIndex]){
-       msg.textContent = "Correct"; 
-      getCurrentQuestion();
-      currentQuestionIndex++;
-   }
-    else 
-    {
-      time = time - 15;
-      msg.textContent = "Incorrect"; 
-      getCurrentQuestion();
-      currentQuestionIndex++;
-      }  
-}
-//    if (currentQuestionIndex = 3 && qChoices){
-// endQuiz();
-// }
 
       
     //retrieves time every second
@@ -111,6 +119,8 @@ function startTimer() {
       time--;
       timerEl.textContent = time;
 
+
+
      if (time <= 0) {
       time = 0;
       timerEl.textContent = 0;
@@ -118,35 +128,43 @@ function startTimer() {
       endQuiz();
       }
     }, 1000);
-  }
-  
+// if (endSection)
+// {  
+//     timerEl.textContent = JSON.stringify(score);
+//   }
 
+}
+ 
   //listens to submit button and stores in local storage
 function submitInit(event){
     event.preventDefault();
-    var answerArray = [];
-    location.href= "hs.html";
-    //gets letters they put in
+    console.log("hello above");
 
+   // 
+
+    //gets letters they put in
+    var answerArray = [];
     localStorage.setItem("initials", initials.value);
-    var newNew2 =  JSON.stringify(localStorage.getItem("initials"));
+    var newNew2 =  localStorage.getItem("initials");
 
 
     localStorage.setItem("score", parseInt(time));
-    var newNew1 = JSON.stringify(localStorage.getItem("score"));
+    var newNew1 = localStorage.getItem("score");
 
-    if(newNew2 != "" && newNew2.length  <= 3){
+    console.log(newNew2);
+    console.log(newNew1);
 
-       newNew2.push(answerArray);
-       newNew1.push(answerArray);
+    if(newNew2 !== "" && newNew2.length  < 7){
+
+       answerArray.push(newNew2);
+       answerArray.push(newNew1);
        
        console.log(answerArray);
-
        
-    //    tableElInit.setAttribute('content', 'test content');
-    //    tableElInit.textContent = newNew2;
-    //    tableElScore.setAttribute('content', 'test content');
-    //    tableElScore.textContent = newNew1;
+       location.href= "hs.html";
+  
+       tableElInit.textContent = newNew2;
+       tableElScore.textContent = newNew1;
     }
 
 
@@ -175,18 +193,6 @@ startButton.addEventListener("click", startQuiz);
 }
 
 
-var b1Click = b1.addEventListener("click", eval);
 
-
-
-var b2Click = b2.addEventListener("click", eval);
-
-    
-
- var b3Click =  b3.addEventListener("click", eval);
-
-
-
- var b4Click = b4.addEventListener("click", eval);
 
 
